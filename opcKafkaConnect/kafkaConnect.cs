589af83e309d651f838d759859a07abd46f595af
panManfredini <pan.manfredini@gmail.com>
2019-10-25 17:28:03 +0200

    first commit

---
+++ opcKafkaConnect/kafkaConnect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpcProxyClient; 
+using Opc.Ua; 
+using OpcProxyCore;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace opcKafkaConnect
+{
+    public class KafkaConnect : IOPCconnect
+    {
+        public void init(JObject config){
+
+        }
+        public void OnNotification(object emitter, MonItemNotificationArgs items){
+
+        }
+        public void setServiceManager(serviceManager serv){
+
+        }
+    }
+}
